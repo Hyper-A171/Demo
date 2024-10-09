62f7449dbf184f4b1950a183b214b1e0dee2d496
Hyper-A171 <Mr.Adarsh.Work@gmail.com>
2024-10-09 21:50:58 +0530

    Creater while for Table

--- Index.py
+++ Index.py
@@ -1,6 +1,17 @@
-conut = 0 
-finalList = []
-while conut <= 3:
-    finalList.append(input())
-    conut += 1
-print(finalList)
+conut = 100
+# finalList = []
+# while conut <= 3:
+#     finalList.append(input())
+#     conut += 1
+# print(finalList)
+
+# conut = 100
+# while conut >= 1 :
+#     print("How to Number Print :-",conut)
+#     conut -=1
+
+n=3
+conut = 1
+while conut  <=10 :
+    print("How to Mulplaying Number :-", n*conut)
+    conut +=1
