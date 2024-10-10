f08ebc8849ba2de9611f8d04dcb6a6748c9a239b
Hyper-A171 <Mr.Adarsh.Work@gmail.com>
2024-10-10 23:30:52 +0530

    Add

--- Index.py
+++ Index.py
@@ -10,8 +10,56 @@ conut = 100
 #     print("How to Number Print :-",conut)
 #     conut -=1
 
-n=3
-conut = 1
-while conut  <=10 :
-    print("How to Mulplaying Number :-", n*conut)
-    conut +=1
+# n=3
+# conut = 1
+# while conut  <=10 :
+#     print("How to Mulplaying Number :-", n*conut)
+#     conut +=1
+
+# def sum (a ,b):
+#     sum_number = a + b
+#     print(sum_number)
+#     return sum_number
+
+# sum(10,20)
+
+# def avg (a,b):
+#     avg_number = (a+b)/2
+#     print(avg_number)
+#     return avg_number
+
+
+# avg(20 ,30)
+
+# list = ["Adarsh","Mishra","Anurag","Mishra","Hariom","Mishra"]
+
+# def print_len():
+#     print(len(list))
+#     return list
+
+# print_len()
+
+
+# def table ():
+#     print("Enter Any Number :- \n")
+#     n = int(input())
+#     i = 1
+#     while i <= 10:
+#         print(n*i)
+#         i += 1
+
+# table()
+
+def table ():
+    print("Enter Any Number :- \n")
+    n = int(input())
+    i = 1
+    while i <= 10:
+        n*=i+i
+        
+        print(n)
+        
+        i += 1
+
+table()
+
